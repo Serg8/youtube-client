270da85dddaa38539c25131401cfd1a3ca5c1cff
Sergey <workintellect@gmail.com>
2018-11-20 10:33:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ const wrapper = wrapperContent();
 const form = formSearch();
 wrapper.appendChild(form);
 body.appendChild(wrapper);
+
+
+  const key = 'AIzaSyDK78nP9NsPif3aF-5men5PIjA7Tysq6qk';
+
+
+  fetch('https://www.googleapis.com/youtube/v3/search?key=' + key + '=snippet&maxResults=15&q=js')
+  .then(function(response) {
+  console.log( response.json());
+  })
+  .catch(function(error) {
+  console.log(error)
+  });
